6986f0550390905f5415ed5400b4d66d6114833d
Rock Chin <1010553892@qq.com>
2021-07-03 23:17:34 +0800

    Add Handler struct

--- src/conn/HandleSocket.go
+++ src/conn/HandleSocket.go
@@ -1,1 +1,18 @@
 package conn
+
+import (
+	"net"
+	"time"
+)
+
+type Handler struct {
+	SID    int    //unique id
+	As     string //client or server
+	Socket net.Conn
+	ConnT  time.Time //timestamp of connection establishing
+	UpV    int       //upload speed
+	DownV  int       //download speed
+	UpD    uint32    //amount of upload data
+	DownD  uint32    //amount of download data
+	Recv   string    //received data
+}
