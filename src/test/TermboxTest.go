9e2cfd54ae85ca7f1872708323b1917586e8f7a1
RockChin <1010553892@qq.com>
2021-07-03 09:52:58 +0800

    Test termbox

---
+++ src/test/TermboxTest.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/nsf/termbox-go"
+)
+
+func main() {
+	termbox.Init()
+	termbox.SetCursor(0, 0)
+	for {
+		eve := termbox.PollEvent()
+		if eve.Err != nil {
+			panic(eve.Err)
+		}
+		fmt.Println(strconv.Itoa(int(eve.Key)) + " ch=" + string(eve.Ch))
+	}
+}
